d6c34ad21ba54d96fddba37ea08e052c8502860f
李九天 <14749974+Swedream-Li@user.noreply.gitee.com>
2024-09-28 16:35:51 +0800

    构建了分支：test_mergeBranch

---
+++ build/CMakeFiles/git_test5_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_test5_1.dir/git_test5_1.cpp.o"
+  "git_test5_1"
+  "git_test5_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_test5_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ git_test5_1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    printf("hello git x 5_1!");
+    return 0;
+}
